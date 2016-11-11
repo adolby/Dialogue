f52f48fa51a532d2a4b695857af4e80ddd93e459
Andrew Dolby <andrewdolby@gmail.com>
2016-11-10 20:59:43 -0500

    Fix Windows build

--- build_windows.cmd
+++ build_windows.cmd
@@ -18,21 +18,6 @@ rd /s /q Dialogue\moc\
 rd /s /q Dialogue\obj\
 rd /s /q Dialogue\qrc\
 
-echo Copying left out Qt files...
-copy "%QT%\bin\Qt5QuickControls2.dll" "Dialogue\Qt5QuickControls2.dll"
-copy "%QT%\bin\Qt5QuickTemplates2.dll" "Dialogue\Qt5QuickTemplates2.dll"
-mkdir Dialogue\Qt\
-mkdir Dialogue\Qt\labs\
-mkdir Dialogue\Qt\labs\settings\
-mkdir Dialogue\QtGraphicalEffects\
-mkdir Dialogue\QtQuick\
-mkdir Dialogue\QtQuick.2\
-robocopy "%QT%\qml\Qt\labs\settings" Dialogue\Qt\labs\settings\ /E
-robocopy "%QT%\qml\QtGraphicalEffects" Dialogue\QtGraphicalEffects\ /E
-robocopy "%QT%\qml\QtQuick" Dialogue\QtQuick\ /E
-robocopy "%QT%\qml\QtQuick.2" Dialogue\QtQuick.2\ /E
-
-
 echo Copying project files for archival...
 copy "%project_dir%\README.md" "Dialogue\README.md"
 copy "%project_dir%\LICENSE" "Dialogue\LICENSE.txt"
